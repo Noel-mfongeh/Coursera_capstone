c27861c4f4836a14cb17c27ac9dbea9b1f831f24
Noel-mfongeh <62423948+Noel-mfongeh@users.noreply.github.com>
2020-03-20 22:05:04 +0200

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook fulfils the requirements of week1 of the capstone project
+
+# In[2]:
+
+
+# importing libaries 
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
